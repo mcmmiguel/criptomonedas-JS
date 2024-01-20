42df65650fc499b5af4b22ae5da37c0f81a78c0e
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-19 20:53:57 -0600

    Leer moneda y cripto seleccionadas

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: '',
+};
 
 // Crear un promise
 const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
@@ -7,6 +14,11 @@ const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 });
 
 function consultarCriptomonedas() {
@@ -25,5 +37,26 @@ function selectCriptomonedas(criptomonedas) {
         option.vlaue = Name;
         option.textContent = FullName;
         criptomonedasSelect.appendChild(option);
-    })
-}
+    });
+};
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+    console.log(objBusqueda);
+};
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if (moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+};
+
+function mostrarAlerta(mensaje) {
+
+};
